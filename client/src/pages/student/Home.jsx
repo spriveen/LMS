585a50ce09fb57ac0aabb1ccac1424f53215d327
spriveen <rpeanuka@gmail.com>
2025-06-28 21:31:15 -0700

    update companies.jsx

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../../components/student/Hero'
+import Companies from '../../components/student/Companies'
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7
     text-center'>
       <Hero />
+      <Companies />
     </div>
   )
 }
