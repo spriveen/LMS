73a678db661e107035b43f2a76b14e08a6dfdb2d
spriveen <rpeanuka@gmail.com>
2025-06-29 23:33:23 -0700

    update coursedetails.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
-
+import humanizeDuration from 'humanize-duration'
 
 export const AppContext = createContext()
 
@@ -30,6 +30,35 @@ export const AppContextProvider =(props)=>{
            return totalRating / course. courseRatings.length
        }
 
+        // function to calculate course course chapter time
+        const calculateChapterTime = (chapter)=>{
+           let time = 0
+           chapter.chapterContent.map((lecture)=> time +=lecture.lectureDuration)
+           return humanizeDuration(time * 60 * 1000,{units:["h","m"]})
+        } 
+        
+        // function to calculate course Duration
+        
+        const calculateCourseDuration = (course)=>{
+         let time = 0
+
+         course.courseContent.map((chapter)=>chapter.chapterContent.map(
+          (lecture) => time += lecture.lectureDuration
+         ))
+          return humanizeDuration(time * 60 * 1000,{units:["h","m"]})
+        }
+
+        //  function calculate to No of Lecture in the course
+        const calculateNoOfLectures = (course)=>{
+           let totalLectures =  0;
+           course.courseContent.forEach(chapter =>{
+            if (Array.isArray(chapter.chapter)){
+              totalLectures += chapter.chapterContent.length
+            }
+           });
+
+           return totalLectures;
+        }
 
     useEffect (()=>{
       fetchAllCourses()
@@ -37,7 +66,8 @@ export const AppContextProvider =(props)=>{
     
     const value = {
         currency, allCourse, navigate,  calculateRating,
-        isEducator, setIsEducator
+        isEducator, setIsEducator, calculateNoOfLectures, calculateCourseDuration,
+         calculateChapterTime
     }
 
     return(
--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -3,11 +3,13 @@ import { useParams } from 'react-router-dom'
 import { AppContext } from '../../context/AppContext'
 import Loading from '../../components/student/Loading'
 import { assets } from '../../assets/assets'
+import humanizeDuration from 'humanize-duration'
 
 const CourseDetails = () => {
   const { id } = useParams()
   const [courseData, setCourseData] = useState(null)
-  const { allCourse ,calculateRating } = useContext(AppContext) // <-- use allCourse
+  const { allCourse, calculateRating, calculateNoOfLectures, calculateCourseDuration,
+    calculateChapterTime } = useContext(AppContext) // <-- use allCourse
 
   useEffect(() => {
     if (allCourse && allCourse.length > 0) {
@@ -27,34 +29,79 @@ const CourseDetails = () => {
           text-course-details-heading-small font-semibold text-gray-800">{courseData.courseTitle}</h1>
           <p
             className="pt-4 md:text-base text-sm"
-            dangerouslySetInnerHTML={{ __html: courseData.courseDescription.slice(0,200)}}>
+            dangerouslySetInnerHTML={{ __html: courseData.courseDescription.slice(0, 200) }}>
           </p>
 
           {/* review and ratings */}
-           <div className='flex items-center space-x-2 pt-3 pb-1 text-sm'>
-                    <p>{calculateRating(courseData)}</p>
-                    <div className='flex'>
-                      {[...Array(5)].map((_, i) => (
-                        <img
-                          key={i}
-                          src={
-                            i < Math.floor(calculateRating(courseData))
-                              ? assets.star
-                              : assets.star_blank
-                          }
-                          alt=''
-                          className='w-3.5 h-3.5'
-                        />
-                      ))}
-                    </div>
-                    <p className='text-blue-600'>({courseData.courseRatings.length}{courseData.courseRatings.length > 1 ? 'ratings' : 'rating'})</p>
+          <div className='flex items-center space-x-2 pt-3 pb-1 text-sm'>
+            <p>{calculateRating(courseData)}</p>
+            <div className='flex'>
+              {[...Array(5)].map((_, i) => (
+                <img
+                  key={i}
+                  src={
+                    i < Math.floor(calculateRating(courseData))
+                      ? assets.star
+                      : assets.star_blank
+                  }
+                  alt=''
+                  className='w-3.5 h-3.5'
+                />
+              ))}
+            </div>
+            <p className='text-blue-600'>({courseData.courseRatings.length}{courseData.courseRatings.length > 1 ? 'ratings' : 'rating'})</p>
 
-                    <p>{courseData.enrolledStudents.length} {courseData.enrolledStudents.length
-                      > 1 ? 'students' : 'student'}</p>
-                  </div>
+            <p>{courseData.enrolledStudents.length} {courseData.enrolledStudents.length
+              > 1 ? 'students' : 'student'}</p>
+          </div>
 
-                  <p className='text-sm'>Course by <span className='text-blue-600 undeine'>Riveen</span></p>
-          
+          <p className='text-sm'>Course by <span className='text-blue-600 undeine'>Riveen</span></p>
+          <div className='pt-8 text-gray-800'>
+           <h2 className='text-xl font-semibold'>Course Structure</h2>
+
+           <div className='pt-5'>
+             {courseData.courseContent.map((chapter , index)=>(
+              <div key={index} className='border border-gray-300 bg-white mb-2
+              rounded'>
+              <div className='flex items-center justify-between px-4 py-3
+              cursor-pointer select-none'>
+                <div className='flex items-center gap-2'>
+                  <img src={assets.down_arrow_icon} alt='arrow icon' />
+                  <p className='font-medium md:text-base text-sm'>{chapter.chapterTitle}</p>
+                </div>
+                <p className='text-sm md:text-default'>{chapter.chapterContent.length} lectures - 
+                  {calculateChapterTime(chapter)} </p>
+              </div>
+                
+                <div className='overflow-hidden transition-all duration-300 
+                max-h-96'>
+                  <ul className='list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600
+                  border-t border-gray-300'>
+                    {chapter.chapterContent.map((lecture, i)=>(
+                       <li key={i} className='flex items-start gap-2 py-1'>
+                        <img src={assets.play_icon} alt='play icon' className='w-4
+                        h-4 mt-1' />
+                        <div className='flex items-center justify-between w-full
+                        text-gray-800 text-xs md:text-default'>
+                          <p>{lecture.lectureTitle}</p>
+                          <div className='flex gap-2'>
+                            {lecture. isPreviewFree && <p className='text-blue-500
+                            cursor-pointer'>Preview</p>}
+                            <p>{humanizeDuration(lecture.lectureDuration *60 * 
+                              1000, {units: ['h', 'm']})}</p>
+                          </div>
+                        </div>
+                       </li>
+                    ))}
+                  </ul>
+                </div>
+
+
+              </div>
+             ))}
+           </div>
+          </div>
+
         </div>
 
         {/* right column */}
