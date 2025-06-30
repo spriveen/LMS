273d0298ff094dca627687bef5306f99b3e76e5b
spriveen <rpeanuka@gmail.com>
2025-06-30 10:14:15 -0700

    update Ratings.jsx

--- client/src/pages/student/Player.jsx
+++ client/src/pages/student/Player.jsx
@@ -5,6 +5,7 @@ import { assets } from '../../assets/assets'
 import humanizeDuration from 'humanize-duration'
 import YouTube from 'react-youtube'
 import Footer from '../../components/student/Footer'
+import Rating from '../../components/student/Rating'
 
 const Player = () => {
   const { enrolledCourse, calculateChapterTime } = useContext(AppContext)
@@ -95,6 +96,7 @@ const Player = () => {
         
           <div className='flex items-center gap-2 py-3 mt-10'>
             <h1 className='text-xl font-bold'>Rate this Course:</h1>
+            <Rating  initialRating={0}/>
           </div>
         </div>
         {/* right column */}
