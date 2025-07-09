2b3bf914757f94f6af42fafda39bd83c10c6150b
spriveen <rpeanuka@gmail.com>
2025-07-09 01:24:55 -0700

    update server.js

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+// Initialize  Express 
+const app = express()
+
+// Middleware
+app.use(cors())
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('API is running')
+})
+
+// Port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
